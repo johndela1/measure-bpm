8a75d6475303506de87d4a0fe83dc6ca5abf2a04
John de la Garza <john@jjdev.com>
2018-12-31 16:48:51 -0800

    rename functions

--- sound.c
+++ sound.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int decayed(unsigned char new, unsigned char old)
+int decay(unsigned char new, unsigned char old)
 {
     double rate = .05;
     return (unsigned char) new*rate + old*(1-rate);
 }
 
-unsigned char rectified(unsigned char v)
+unsigned char rectify(unsigned char v)
 {
     //printf("after: %d\n", v < 127 ? 127 - v : v - 127);
     return v < 127 ? 127 - v : v - 127;
@@ -19,11 +19,11 @@ int main()
     int sound = 0;
     int avg = 0;
     for (int t1 = 0, t2 = 0; !feof(stdin); t2++) {
-        v1 = v = decayed(rectified(fgetc(stdin)), v1);
+        v1 = v = decay(rectify(fgetc(stdin)), v1);
 
-        if (t2 % 100 == 0) printf("voltage %d\n", v);
+        // if (t2 % 10000== 0) printf("\n");
 
-        if (v > 20) {
+        if (v > 15) {
             if (!sound) {
                 t1 = t2;
                 printf("start %d\n", t1);
@@ -32,7 +32,7 @@ int main()
         }
         else {
             if (sound) {
-                printf("duration: %d\n", t2-t1);
+                // printf("end %d\ndur  %d\n\n", t2, t2-t1);
                 sound = 0;
             }
         }
