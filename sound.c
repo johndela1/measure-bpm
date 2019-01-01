d92b1ec6171ff56b42065fb819825a45ff0e8cce
John de la Garza <john@jjdev.com>
2019-01-01 09:12:14 -0800

    use average

--- sound.c
+++ sound.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int _avg(unsigned char new, unsigned char old)
+static int _avg(int new, int old)
 {
-    double rate = .05;
-    return (unsigned char) new*rate + old*(1-rate);
+    double rate = .75;
+    return (int) (new*rate + old*(1-rate));
 }
 
 static int rectify(int v)
@@ -15,15 +14,18 @@ static int rectify(int v)
 int main()
 {
     int threshold = 20;
-    int v = 0;
+    int v;
     int decay = 0;
+    int dur;
+    int avgdur = 0;
 
     for (int t1 = 0, t2 = 0; feof(stdin) == 0; t2++) {
         v = rectify(fgetc(stdin));
-
         // if (t2 % 100== 0) printf(": %d\n", v);
         if (v > threshold && decay == 0) {
-            printf("beat %d\n", (int) (t2-t1) / 100);
+            dur = (int) ((t2-t1) / 100);
+            avgdur = _avg(dur, avgdur);
+            printf("beat %d %d\n", dur, avgdur);
             t1 = t2;
             decay = 3000;
         }
