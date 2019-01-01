f7651db99bdfff1327d364010a1cb47b1ca3ebba
John de la Garza <john@jjdev.com>
2019-01-01 15:12:47 -0800

    add bpm output

--- sound.c
+++ sound.c
@@ -14,18 +14,20 @@ static int rectify(int v)
 int main()
 {
     int threshold = 20;
-    int v;
     int decay = 0;
-    int dur;
-    int avgdur = 0;
+    int bpm, period, t1, t2, v;
+    int avgperiod = 0;
+    int hz = 8000;
 
-    for (int t1 = 0, t2 = 0; feof(stdin) == 0; t2++) {
+    for (t1 = t2 = 0; feof(stdin) == 0; t2++) {
         v = rectify(fgetc(stdin));
-        // if (t2 % 100== 0) printf(": %d\n", v);
+        // if (t2 % 1000== 0) printf(": %d\n", (t2-t1)>>1);
         if (v > threshold && decay == 0) {
-            dur = (int) ((t2-t1) / 100);
-            avgdur = _avg(dur, avgdur);
-            printf("beat %d %d\n", dur, avgdur);
+            period = t2-t1;
+            bpm = (int) (60 / (((double)(t2-t1)) / hz));
+            avgperiod = _avg(period, avgperiod);
+            printf("period %d %d\n", period, avgperiod);
+            printf("bpm %d\n", bpm);
             t1 = t2;
             decay = 3000;
         }
